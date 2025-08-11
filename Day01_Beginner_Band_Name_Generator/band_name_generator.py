6312bd81154d63db2053d7654600670d057b2d9e
Chirag Dhawan <104852727+chiragdhawan07@users.noreply.github.com>
2025-08-11 14:10:05 +0530

    band_name_generator.py

---
+++ Day01_Beginner_Band_Name_Generator/band_name_generator.py
@@ -0,0 +1,13 @@
+# Day 1 - Beginner Band Name Generator
+
+# Greet the user
+print("Welcome to the Band Name Generator!")
+
+# Ask the user for the city they grew up in
+city = input("What's the name of the city you grew up in?\n")
+
+# Ask the user for the name of their pet
+pet = input("What's your pet's name?\n")
+
+# Combine the city and pet name to suggest a band name
+print("Your band name could be " + city + " " + pet)
