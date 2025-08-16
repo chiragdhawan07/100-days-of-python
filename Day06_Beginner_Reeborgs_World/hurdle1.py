9e98de106536c19e365b7171a39da83d1355dae8
Chirag Dhawan <104852727+chiragdhawan07@users.noreply.github.com>
2025-08-16 15:25:07 +0530

    hurdle1.py

---
+++ Day06_Beginner_Reeborgs_World/hurdle1.py
@@ -0,0 +1,19 @@
+# Day 6 – Reeborg’s World: Hurdle 1
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+def jump():
+    move()
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+
+for step in range(6):
+    jump()
