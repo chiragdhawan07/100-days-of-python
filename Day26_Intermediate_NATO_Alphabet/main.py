998566f639ff02490648b30509a249c219408ede
Chirag Dhawan <104852727+chiragdhawan07@users.noreply.github.com>
2025-09-10 11:53:35 +0530

    main.py

---
+++ Day26_Intermediate_NATO_Alphabet/main.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+# Read the CSV file
+data = pd.read_csv("NATO-alphabet-start/nato_phonetic_alphabet.csv")
+
+# Create dictionary: letter → code
+nato_dict = {row.letter: row.code for (index, row) in data.iterrows()}
+
+# Ask user for a word
+word = input("Enter a word: ").upper()
+
+# Convert each letter into NATO code
+result = [nato_dict[letter] for letter in word if letter in nato_dict]
+
+print(result)
